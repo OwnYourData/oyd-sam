c2e637aa1dab1408be7d20743d788e92ba87b366
Christoph Fabianek <christoph.fabianek@gmail.com>
2017-02-24 15:41:03 +0100

    debugging

--- docker/oyd-sam/script/run.sh
+++ docker/oyd-sam/script/run.sh
@@ -2,12 +2,12 @@
 
 #start postgreSQL
 echo "starting postgreSQL ... "
-su postgres -c "postgres -D /var/lib/postgresql/data &"
+su postgres -c "/usr/lib/postgresql/9.4/bin/postgres -D /var/lib/postgresql/data &"
 sleep 5
 
 echo "creating DB"
-su postgres -c "psql --command \"ALTER USER postgres WITH SUPERUSER PASSWORD 'postgres';\""
-su postgres -c "createdb postgres"
+su postgres -c "/usr/lib/postgresql/9.4/bin/psql --command \"ALTER USER postgres WITH SUPERUSER PASSWORD 'postgres';\""
+su postgres -c "/usr/lib/postgresql/9.4/bin/createdb postgres"
 
 echo "starting SAM"
 cd /oyd-sam
